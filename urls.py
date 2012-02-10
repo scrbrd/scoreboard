ca496710ad5d3b94718c86b1206a406adfa16284
Evan Hammer <hammer@sqoreboard.com>
2012-02-10 00:53:25 +0000

    facebook login testing - not in a great state but useful learning material

--- urls.py
+++ urls.py
@@ -3,6 +3,7 @@ from handlers.login import LoginHandler
 from handlers.games import GamesHandler
 from handlers.rankings import RankingsHandler
 from handlers.create import CreateGameHandler
+from handlers.facebooktest import FacebookTestHandler
 
 url_patterns = [
     (r"/", HomeHandler),
@@ -10,4 +11,5 @@ url_patterns = [
     (r"/games", GamesHandler),
     (r"/rankings", RankingsHandler),
     (r"/create/game", CreateGameHandler),
+    (r"/", FacebookTestHandler),
 ]
