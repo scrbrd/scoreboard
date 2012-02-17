45185d11be6a557e74eed0ec3b25faed6df597dc
Jon Warman <warman@sqoreboard.com>
2012-02-17 21:31:10 +0000

    WIP

--- model/app/catchers.py
+++ model/app/catchers.py
@@ -9,14 +9,20 @@ from model.api import Opponent
 
 
 def generate_rankings(league_id):
+    """ Generate league rankings based on most wins.
+
+    Required:
+    id  league_id  League node id
+
+    Return:
+    dict of name/wins tuples keyed on opponent id.
+
     """
-    
-    """
-    rankings = {}
 
     league = League.load_opponents(league_id)
 
+    rankings = {}
     for opponent in league.get_opponents():
-        rankings[opponent.id()] = opponent.count_wins()
+        rankings[opponent.id()] = (opponent.name(), opponent.count_wins())
     return rankings
 
